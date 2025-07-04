1de56352389a9fbeccee6fa3055ed36100dbac8f
HappyAdebayo <ogbenihappy05@gmail.com>
2025-07-04 12:13:48 -0700

    done

--- config.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def get_db_connection():
-    return psycopg2.connect(
-        host="localhost",
-        database="pricepeepsdb",
-        user="postgres",
-        password="adeyanju#2005"
-    )
